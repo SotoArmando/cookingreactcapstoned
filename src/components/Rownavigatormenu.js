11a120092523026e4b198441d5a68f8882cdafbd
Sotoarmando <armando29@live.com>
2021-06-15 04:59:26 -0400

    Added sign out func

--- src/components/Rownavigatormenu.js
+++ src/components/Rownavigatormenu.js
@@ -3,8 +3,9 @@ import { withRouter } from "react-router";
 import { createMapDispatchtoProps } from "../reducers/createDefaultreducer";
 import { connect } from "react-redux";
 
-function Rownavigatormenu({ location: { pathname }, history, history: { length: historylength }, session: { active } }) {
+function Rownavigatormenu({ location: { pathname }, history, history: { length: historylength }, session: { active }, u_session }) {
 
+    const handleSignout = () => u_session('active', false)
     const handleClick = (string) => {
         switch (string) {
             case ("Assistant"): history.push('/assistant'); break;
@@ -13,6 +14,7 @@ function Rownavigatormenu({ location: { pathname }, history, history: { length: 
             case ("Save"): history.push('/profile/library'); break;
             case ("Sign"): history.push('/sign'); break;
             case ("Back"): history.goBack(); break;
+            case ("Sign out"): handleSignout(); break;
             default: history.push('/'); break;
         }
     }
@@ -30,15 +32,15 @@ function Rownavigatormenu({ location: { pathname }, history, history: { length: 
         </div>
         <div className="row pad_r24">
             {
-                [["Home",4], ["Assistant",7], ["Sign",4]].filter((e,i) => [true, true, !active][i] ).
-                    map(([e,size]) => <span onClick={() => handleClick(e)} className={`corebox_x${size} center btn_u tcenter`}>{e}</span>)
+                [["Home", 4], ["Assistant", 7], ["Sign", 4]].filter((e, i) => [true, true, !active][i]).
+                    map(([e, size]) => <span onClick={() => handleClick(e)} className={`corebox_x${size} center btn_u tcenter`}>{e}</span>)
             }
             {active ? <Cellprofilepicture size={30} /> : []}
             <span className="corebox_x6 corebox_3 center  btn_u hover relative">
                 {active ? 'Profile' : 'Session'}
                 <div className="to_hover absolute  corebox_x6  col right marcore_t26">
                     {
-                        ["Settings", "Library"].
+                        ["Settings", "Library", "Sign out"].
                             map(e => <span onClick={() => handleClick(e)} className="corebox_3 corebox_x5 center btn_u back_0">{e}</span>)
                     }
                 </div>
