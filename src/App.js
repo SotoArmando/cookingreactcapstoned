4317f20982cb5cb79d140718872fc2947048c7ef
Sotoarmando <armando29@live.com>
2021-09-16 07:22:42 +0000

    Removed TODO features from components, containers

--- src/App.js
+++ src/App.js
@@ -6,24 +6,12 @@ import {
 import { connect } from 'react-redux';
 import { createMapDispatchtoProps } from './reducers/createDefaultreducer';
 import Portraitmeals from './components/Portraitmeals';
-import Portraitprofile from './components/Portraitprofile';
 import Rownavigatormenu from './components/Rownavigatormenu';
-import Portraitprofilesettings from './components/Portraitprofilesettings';
-import Portraitprofilelibrary from './components/Portraitprofilelibrary';
-import Portraitassistant from './components/Portraitassistant';
-import Portraitassistantrecipe from './components/Portraitassistantrecipe';
-import Portraitsign from './components/Portraitsign';
 import Homepath from './components/Portraithomepath';
 
 function App() {
   const paths = {
     '/recipe/:id': Portraitmeals,
-    '/profile/library': Portraitprofilelibrary,
-    '/profile/settings': Portraitprofilesettings,
-    '/profile/:id': Portraitprofile,
-    '/assistant/recipe/:id': Portraitassistantrecipe,
-    '/assistant': Portraitassistant,
-    '/sign': Portraitsign,
     '/': connect(() => ({}), createMapDispatchtoProps())(Homepath),
   };
 
